d819061f4657ceb10ad6948e35a5e4263b156a04
Harunur Rashid <142937596+harunur6729@users.noreply.github.com>
2025-01-08 09:23:19 +0600

    password hide and unhide option added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,22 +1,41 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/firebase.config";
 import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const Register = () => {
   const [registerError, setRegisterError] = useState(null);
-    const handleRegister = e =>{
+  const [success , setSuccess] = useState('');
+  const [showPassword, setShowPassword] =useState(false)
+  const handleRegister = (e) =>{
       e.preventDefault();
       const email = e.target.email.value;
       const password = e.target.password.value;
       console.log(email , password)
+
       //reset error
       setRegisterError('') //jodi reset na kori tahole state ar vitor error ta roe jabe;
+
+      if(password.length<6){
+        setRegisterError('Password should be at leaset 6 characters or longer');
+        return;//jehetu 6 char pass na hole faild korbei tahole sudhu sudhu samne dike jete dewar ki dorkar.tai return kore dilam jate akhan thekei fire jai
+      }
+      else if(!/[A-Z]/.test(password)){
+        setRegisterError('Your password should have at least one uper case characters.')
+        return;
+      }
+
+      
 
       // cereate user
       createUserWithEmailAndPassword(auth, email, password)
       .then(userCredential =>{
         const user = userCredential.user;
         console.log(user);
+        setSuccess('User Created Successfully');
+        toast.success(success);
+        
       })
       .catch(error =>{
         console.error(error);
@@ -25,15 +44,37 @@ const Register = () => {
     }
   return (
     <div className="relative top-40  ">
+      <ToastContainer></ToastContainer>
       <div className="mx-auto md:w-1/2  bg-yellow-50 text-center rounded-lg p-10">
         <h2 className="text-3xl mb-6 text-black font-semibold">Please Register:</h2>
-        <form onSubmit={handleRegister}>
-          <input className="mb-4 w-3/4 bg-white border rounded-md py-2 px-4" type="email" name="email" id="" placeholder="Enter your email..."/>
+        <div className="flex justify-center">
+        <form className="w-1/2 " onSubmit={handleRegister}>
+          <input className="mb-4 w-full bg-white border rounded-md py-2 px-4" type="email" name="email" id="" required placeholder="Enter your email..."/>
           <br />
-          <input className="mb--4 w-3/4 py-2 px-4 bg-white border rounded-md" type="password" name="password" id=""  placeholder="Enter your password..."/>
+
+          {/* password field  */}
+         
+         <div className=" relative  rounded-md">
+         <input
+           className=" w-full py-2 px-4 bg-white border rounded-md" 
+           type={showPassword? "text":"password"} //jodi showPassword true hoi tahole type hobe text r false hole type hobe password
+           name="password" 
+           id=""  
+           required  
+           placeholder="Enter your password..."/>
+           <span className="absolute top-2 right-2 text-2xl" onClick={ () => setShowPassword(!showPassword)}>
+            {
+              showPassword?<FaEye></FaEye>:<FaEyeSlash></FaEyeSlash>
+            }
+            </span> 
+            {/**(!showPassword)atar mane holo click korar por true thakle false hobe r false thakle true hobe ,atar jonno akta user state use  kora hoeche*/}
+         </div>
+         
+
           <br />
-          <input className=" mt-4 btn btn-primary w-3/4" type="submit" value="Register" />
+          <input className="  btn btn-primary w-1/2" type="submit" value="Register" />
         </form>
+        </div>
         {
           registerError && <p className="text-red-400">{registerError}</p>
         }
